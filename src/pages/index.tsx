986b55166df2d810408918262fdf30c1b8c1eeaa
Naman Anand <iamnamananand996@gmail.com>
2024-03-10 13:50:33 +0530

    chore: comit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,114 +5,186 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="h-full relative bg-white">
+      <div className="w-full h-[800px] left-[-1px] top-0 absolute">
+        <img
+          className="h-[800px] left-0 top-0 absolute"
+          src="/bg-hero.svg"
+        />
+        <div className="w-full h-[699px] left-[24px] top-[78px] absolute">
+          <div className="left-[345px] top-[369px] absolute text-zinc-950 text-4xl font-normal font-['Inter']">
+            Everything About Cricket
+          </div>
+          <div className="w-full left-[343px] top-[282px] absolute text-black text-7xl font-bold font-['Inter']">
+            Cricscroll
+          </div>
+          <div className="w-10 h-[60px] left-[1890px] top-[296px] absolute" />
+          <div className="w-10 h-[60px] left-0 top-[292px] absolute" />
+          <div className="w-[195px] h-[180px] left-[345px] top-0 absolute">
+            <img
+              className="w-[195px] h-[180px] left-0 top-0 absolute rounded-full"
+              src="https://via.placeholder.com/195x180"
             />
-          </a>
+            <div className="left-[40px] top-[61px] absolute text-neutral-400 text-opacity-0 text-5xl font-normal font-['Inter']">
+              Logo
+            </div>
+          </div>
+          <div className="w-[22px] h-[22px] left-[917px] top-[677px] absolute bg-white rounded-full" />
+          <div className="w-[22px] h-[22px] left-[954px] top-[677px] absolute bg-neutral-400 rounded-full" />
+          <div className="w-[22px] h-[22px] left-[993px] top-[677px] absolute bg-neutral-400 rounded-full" />
+          <div className="w-[22px] h-[22px] left-[1031px] top-[677px] absolute bg-neutral-400 rounded-full" />
+        </div>
+        <div className="w-[1124px] h-[100px] left-[496px] top-[110px] absolute">
+          <div className="w-[929px] h-[60px] left-[195px] top-[19px] absolute bg-yellow-950" />
+          <div className="left-[945px] top-[35px] absolute text-white text-2xl font-normal font-['Inter']">
+            Purchase
+          </div>
+          <div className="left-[570px] top-[35px] absolute text-white text-2xl font-normal font-['Inter']">
+            Features
+          </div>
+          <div className="left-[743px] top-[35px] absolute text-stone-50 text-2xl font-normal font-['Inter']">
+            Contact Us
+          </div>
+          <div className="left-[195px] top-[44px] absolute text-black text-2xl font-normal font-['Inter']">
+            {" "}
+          </div>
+          <div className="left-[394px] top-[35px] absolute text-white text-2xl font-normal font-['Inter']">
+            About us
+          </div>
+          <div className="w-[85px] left-[233px] top-[35px] absolute text-white text-2xl font-normal font-['Inter']">
+            Home
+          </div>
+          <div className="w-[100px] h-[100px] left-0 top-0 absolute" />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="w-[1980px] h-[760px] left-[2px] top-[1666px] absolute">
+        <img
+          className="w-[1980px] h-[760px] left-0 top-0 absolute"
+          src="/bg-hero-2.svg"
         />
+        <div className="w-[886px] h-[479px] left-[728px] top-[178px] absolute">
+          <div className="left-[681px] top-[432px] absolute text-stone-50 text-[32px] font-normal font-['Inter']">
+            Read
+          </div>
+          <div className="left-[288px] top-[440px] absolute text-white text-[32px] font-normal font-['Inter']">
+            Read
+          </div>
+          <div className="w-96 h-[60.80px] left-[135px] top-[58px] absolute text-center text-black text-opacity-95 text-[32px] font-normal font-['Inter']">
+            Check out
+          </div>
+          <div className="w-[654px] left-0 top-0 absolute text-center text-black text-5xl font-bold font-['Inter']">
+            Other Articles{" "}
+          </div>
+          <img
+            className="w-[300px] h-[247px] left-[188px] top-[193px] absolute"
+            src="https://via.placeholder.com/300x247"
+          />
+          <img
+            className="w-[298px] h-[247px] left-[588px] top-[193px] absolute"
+            src="https://via.placeholder.com/298x247"
+          />
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="w-[1979px] h-[866px] left-0 top-[800px] absolute">
+        <div className="w-[1979px] h-[866px] left-0 top-0 absolute bg-yellow-200" />
+        <div className="w-[715px] h-[283px] left-[889px] top-[312px] absolute text-yellow-950 text-opacity-95 text-2xl font-normal font-['Inter']">
+          Over the years as the World Cup has evolved to be a bigger and better
+          platform for competing for the ultimate glory in the world of cricket,
+          records are being made and broken at will and weird instances and
+          events continue to dot the global spectrum of international cricket.
+          Here are some of the most interesting facts about the ICC World Cup
+          from its glorious years in history
+        </div>
+        <div className="left-[889px] top-[251px] absolute text-black text-opacity-95 text-[40px] font-normal font-['Inter']">
+          10 facts you should know about world cup
+        </div>
+        <div className="left-[893px] top-[164px] absolute text-black text-5xl font-normal font-['Inter']">
+          ICC 2023 ODI WC is around the corner{" "}
+        </div>
+        <img
+          className="w-[405px] h-[405px] left-[368px] top-[190px] absolute"
+          src="https://via.placeholder.com/405x405"
+        />
+        <div className="left-[926px] top-[611px] absolute text-neutral-50 text-2xl font-normal font-['Inter']">
+          Read More
+        </div>
       </div>
-    </main>
+      <div className="w-[1978px] h-[844px] left-[2px] top-[2426px] absolute">
+        <img
+          className="w-[1978px] h-[844px] left-0 top-0 absolute"
+          src="https://via.placeholder.com/1978x844"
+        />
+        <div className="w-[195px] h-[203.10px] left-[1206px] top-[84.63px] absolute">
+          <img
+            className="w-[195px] h-[203.10px] left-0 top-0 absolute rounded-full"
+            src="https://via.placeholder.com/195x203"
+          />
+          <div className="w-[114px] h-[65.44px] left-[40px] top-[68.83px] absolute text-neutral-400 text-opacity-0 text-5xl font-normal font-['Inter']">
+            Logo
+          </div>
+        </div>
+        <div className="w-[405px] h-[49.65px] left-[366px] top-[186.18px] absolute text-blue-950 text-4xl font-normal font-['Inter']">
+          Send us a message
+        </div>
+        <div className="w-[405px] h-[78.98px] left-[366px] top-[91.40px] absolute text-black text-[58px] font-bold font-['Inter']">
+          Contact us
+        </div>
+        <div className="w-[405px] h-[78.98px] left-[782px] top-[91.40px] absolute text-black text-[58px] font-bold font-['Inter']">
+          Sitemap
+        </div>
+        <div className="w-[405px] h-[49.65px] left-[784px] top-[186.18px] absolute text-black text-4xl font-normal font-['Inter']">
+          All our pages
+        </div>
+        <div className="w-[405px] h-[53px] left-[1206px] top-[369px] absolute">
+          <div className="w-[405px] h-[49.65px] left-0 top-[3.35px] absolute text-white text-4xl font-normal font-['Inter']">
+            {" "}
+            (+91) 786 6487 5
+          </div>
+        </div>
+        <div className="w-[405px] h-[49.65px] left-[1206px] top-[468.26px] absolute">
+          <div className="w-9 h-[40.62px] left-[10px] top-[4.51px] absolute" />
+          <div className="w-[405px] h-[49.65px] left-0 top-0 absolute text-white text-4xl font-normal font-['Inter']">
+            {" "}
+            mail@gmail.com
+          </div>
+        </div>
+        <div className="w-[298px] h-[67.61px] left-[368px] top-[380.56px] absolute bg-white" />
+        <div className="w-[405px] h-[139.91px] left-[366px] top-[475.03px] absolute bg-white" />
+        <div className="w-[119px] h-[32.72px] left-[409px] top-[405.07px] absolute text-neutral-600 text-opacity-95 text-2xl font-normal font-['Inter']">
+          Your Email
+        </div>
+        <div className="w-[161px] h-[32.72px] left-[409px] top-[496.47px] absolute text-neutral-600 text-opacity-95 text-2xl font-normal font-['Inter']">
+          Your Message
+        </div>
+        <div className="w-[300px] h-[65.43px] left-[366px] top-[288.97px] absolute bg-white" />
+        <div className="w-[108px] h-[31.62px] left-[409px] top-[305.32px] absolute text-neutral-600 text-opacity-95 text-2xl font-normal font-['Inter']">
+          Full name
+        </div>
+        <div className="w-[193px] h-[65.43px] left-[366px] top-[635px] absolute bg-neutral-700" />
+        <div className="w-[119px] h-[32.72px] left-[410px] top-[652.08px] absolute text-white text-2xl font-normal font-['Inter']">
+          Submit
+        </div>
+        <div className="w-[197px] h-[173.93px] left-[781px] top-[266.07px] absolute">
+          <div className="w-[191px] h-[43px] left-[3px] top-[130.93px] absolute text-black text-4xl font-normal font-['Inter']">
+            -Purchase
+          </div>
+          <div className="w-[191px] h-[25.30px] left-[6px] top-[89.93px] absolute text-black text-4xl font-normal font-['Inter']">
+            -Feature
+          </div>
+          <div className="w-[197px] h-[25.30px] left-0 top-[44.74px] absolute text-black text-4xl font-normal font-['Inter']">
+            -About Us
+          </div>
+          <div className="w-[119px] h-[25.31px] left-[6px] top-0 absolute text-black text-4xl font-normal font-['Inter']">
+            -Home
+          </div>
+        </div>
+      </div>
+      <div className="w-[1979px] h-[127px] left-0 top-[3143px] absolute">
+        <div className="w-[1979px] h-[127px] left-0 top-0 absolute bg-black" />
+        <div className="left-[615px] top-[47px] absolute text-white text-[28px] font-normal font-['Inter']">
+          Copyright 2023 All Rights Reserved Company Name
+        </div>
+      </div>
+    </div>
   );
 }
