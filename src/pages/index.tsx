52d10c9553735f1cdfe982dd91b61e2177ea0971
Naman Anand <iamnamananand996@gmail.com>
2024-03-26 01:38:48 +0530

    chore: update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,153 +18,9 @@ export default function Home() {
                   <img src="/logo.png" alt="logo" />
                 </a>
               </div>
-              <div className="hidden md:block">
-                <div className="ml-4 flex items-center space-x-4">
-                  <a
-                    href="/"
-                    className="text-white hover:bg-white hover:text-black rounded-lg p-2"
-                  >
-                    Home
-                  </a>
-                  <a
-                    href="/"
-                    className="text-white hover:bg-white hover:text-black rounded-lg p-2"
-                  >
-                    Services
-                  </a>
-                  <a
-                    href="/"
-                    className="text-white hover:bg-white hover:text-black rounded-lg p-2"
-                  >
-                    Platforms
-                  </a>
-                  <a
-                    href="/"
-                    className="text-white hover:bg-white hover:text-black rounded-lg p-2"
-                  >
-                    About Us
-                  </a>
-                  <a
-                    href="/"
-                    className="text-white hover:bg-white hover:text-black rounded-lg p-2"
-                  >
-                    Sports Id
-                  </a>
-                  <a
-                    href="/"
-                    className="text-white hover:bg-white hover:text-black rounded-lg p-2"
-                  >
-                    Contact Us
-                  </a>
-                  <a
-                    href="/"
-                    className="text-white hover:bg-white hover:text-black rounded-lg p-2"
-                  >
-                    Blog
-                  </a>
-                  <a
-                    href="/"
-                    className="text-white hover:bg-white hover:text-black rounded-lg p-2"
-                  >
-                    Rule
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="md:hidden">
-              <button
-                className="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                onClick={toggleNavbar}
-              >
-                {isClick ? (
-                  <svg
-                    className="h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    className="h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M4 6h16M4 12h16m-7 6h7"
-                    />
-                  </svg>
-                )}
-              </button>
             </div>
           </div>
         </div>
-        {isClick && (
-          <div className="md:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-              <a
-                href="/adventures"
-                className="text-white block hover:bg-white hover:text-black rounded-lg p-2"
-              >
-                Home
-              </a>
-              <a
-                href="/destinations"
-                className="text-white block hover:bg-white hover:text-black rounded-lg p-2"
-              >
-                Services
-              </a>
-              <a
-                href="/booking"
-                className="text-white block hover:bg-white hover:text-black rounded-lg p-2"
-              >
-                Platforms
-              </a>
-              <a
-                href="/profile"
-                className="text-white block hover:bg-white hover:text-black rounded-lg p-2"
-              >
-                About Us
-              </a>
-              <a
-                href="/contact-us"
-                className="text-white block hover:bg-white hover:text-black rounded-lg p-2"
-              >
-                Sports Id
-              </a>
-              <a
-                href="/contact-us"
-                className="text-white block hover:bg-white hover:text-black rounded-lg p-2"
-              >
-                Contact Us
-              </a>
-              <a
-                href="/contact-us"
-                className="text-white block hover:bg-white hover:text-black rounded-lg p-2"
-              >
-                Blog
-              </a>
-              <a
-                href="/contact-us"
-                className="text-white block hover:bg-white hover:text-black rounded-lg p-2"
-              >
-                Rule
-              </a>
-            </div>
-          </div>
-        )}
       </nav>
 
       {/* Main Content */}
@@ -203,7 +59,7 @@ export default function Home() {
             <div className="md:w-1/2">
               <img
                 loading="lazy"
-                srcSet="/play.png"
+                srcSet="/banner.png"
                 className="w-full"
                 alt="Play"
               />
